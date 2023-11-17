fce27e5c377931d1b1917e83d31f0ef719031888
Ej Myles <ejelymyles@gmail.com>
2023-11-16 22:30:02 -0800

    finish lab

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,17 @@
-class Tree:
+
   def __init__(self, root = None):
     self.root = root
 
   def get_element_by_id(self, id):
+    if not self.root:
+      return None
+
+    if self.root.get('id') == id:
+      return self.root
+
+    for child in self.root.get('children', []):
+      result = Tree(root=child).get_element_by_id(id)
+      if result:
+        return result 
+
     pass
